0333159500784c7051f38264a0e6ab2a5014844d
ngphsotu <ngphsotu@gmail.com>
2023-04-24 23:27:28 +0700

    Delete unnecessary

---
+++ lib/app/home/view/home_page.dart
@@ -0,0 +1,10 @@
+import '/lib.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Hello World')));
+  }
+}
